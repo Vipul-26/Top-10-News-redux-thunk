be450ab43e87dd1804893d4c8d13556b3dfa66bf
Vipul-26 <vipulsinghssm1@gmail.com>
2022-07-25 09:35:39 +0530

    Revert Header Changes

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,13 +15,7 @@ export const receivedPosts = (data) => ({
 export const fetchPosts = (channelName) => {
   return function (dispatch) {
     dispatch(requestPosts());
-    return fetch(`https://newsapi.org/v1/articles?source=${channelName}&apiKey=${process.env.REACT_APP_API_KEY || 'c40d4dc492a8434a9b91a932f745989a'}`,
-      {
-        headers: {
-          Upgrade: "HTTP / 3.0",
-          Connection: "Upgrade",
-        }
-      })
+    return fetch(`https://newsapi.org/v1/articles?source=${channelName}&apiKey=${process.env.REACT_APP_API_KEY}`)
       .then(
         (response) => response.json(),
         (error) => console.log('An error occurred.', error),
