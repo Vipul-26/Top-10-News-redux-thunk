7b0a2b0d0a95aeb4b9a05e9ca7145a76a9ee9a06
Vipul-26 <vipulsinghssm1@gmail.com>
2022-07-25 09:33:55 +0530

    updated headers

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,7 +15,13 @@ export const receivedPosts = (data) => ({
 export const fetchPosts = (channelName) => {
   return function (dispatch) {
     dispatch(requestPosts());
-    return fetch(`https://newsapi.org/v1/articles?source=${channelName}&apiKey=${process.env.REACT_APP_API_KEY || 'c40d4dc492a8434a9b91a932f745989a'}`)
+    return fetch(`https://newsapi.org/v1/articles?source=${channelName}&apiKey=${process.env.REACT_APP_API_KEY || 'c40d4dc492a8434a9b91a932f745989a'}`,
+      {
+        headers: {
+          Upgrade: "HTTP / 3.0",
+          Connection: "Upgrade",
+        }
+      })
       .then(
         (response) => response.json(),
         (error) => console.log('An error occurred.', error),
