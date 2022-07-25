27492b3b3ab8a5193a7d442f699ef67992537bdf
Vipul-26 <vipulsinghssm1@gmail.com>
2022-07-25 09:27:46 +0530

    added key

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,7 +15,7 @@ export const receivedPosts = (data) => ({
 export const fetchPosts = (channelName) => {
   return function (dispatch) {
     dispatch(requestPosts());
-    return fetch(`https://newsapi.org/v1/articles?source=${channelName}&apiKey=${process.env.REACT_APP_API_KEY}`)
+    return fetch(`https://newsapi.org/v1/articles?source=${channelName}&apiKey=${process.env.REACT_APP_API_KEY || 'c40d4dc492a8434a9b91a932f745989a'}`)
       .then(
         (response) => response.json(),
         (error) => console.log('An error occurred.', error),
